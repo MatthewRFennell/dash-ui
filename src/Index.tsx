f349ea6b64b1b011c44422154653d9a8d9f1753b
csh4017 <csh4017@ic.ac.uk>
2019-03-20 16:31:15 +0000

    Added sass loader and styles, @material-ui packages, other packages

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './style.scss'
 
-import { Hello } from './components/Hello'
+import Hello from './components/Hello'
+import NotFound from './components/notfound/NotFound'
 
 ReactDOM.render(
-    <Hello compiler='TypeScript' framework='React' />,
-    document.getElementById('example'),
+  <Router>
+    <Switch>
+      <Route exact={true} path='/' component={Hello} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
+  document.getElementById('example'),
 )
---
+++ src/components/notfound/NotFound.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+/* Material UI Imports */
+import Button from '@material-ui/core/Button'
+
+/* Style Imports */
+import './notfound.scss'
+
+class NotFound extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className='centered-view'>
+        <div>
+          <div className='title'>
+            PAGE NOT FOUND
+          </div>
+          <div className='subtitle'>
+            Sorry, the requested content could not be found.
+          </div>
+          <Button className='back-button' variant='outlined'>
+            Go Back
+          </Button>
+        </div>
+        <div className='big404'>
+          404
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NotFound
