d589a1ddc7ad70031c4e2f4b7125481409106ee8
rzj17 <robert.jin17@ic.ac.uk>
2019-03-20 16:06:23 +0000

    Replace deprecated SFC with FunctionComponent

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,7 +4,7 @@ export interface HelloProps { compiler: string; framework: string }
 
 // 'HelloProps' describes the shape of props.
 // Stateless function component so SFC is used.
-export const Hello : React.SFC<HelloProps> = (props) => {
+export const Hello : React.FunctionComponent<HelloProps> = (props) => {
     return (
         <h1>Hello from {props.compiler} and {props.framework}!</h1>
     )
