ef2e9524a53e57c1fdd04f01df65825887a26210
Luke <lukebenjaminely@gmail.com>
2019-05-30 17:35:40 +0100

    Added functions for editing event details

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -15,20 +15,44 @@ import { CreateEvent } from './modal/CreateEvent'
 const placeholderImage = require('../../../assets/png/care-bears.jpg')
 
 const Dashboard: React.FunctionComponent<DashboardProps> = (props: DashboardProps) => {
-  
 
   const [openEvent, setOpenEvent] = React.useState<EventFullDetails | undefined>(undefined)
   const [modalOpen, setModalOpen] = React.useState<boolean>(false)
 
   const updateTransport = (transport) => {
-    fetchProtected(DASH_API + '/editTansport', null, transport, 'PUT', ((res) => {
+    fetchProtected(DASH_API + '/editTansport', null, {...transport, id: openEvent.event_id}, 'PUT', ((res) => {
       if (res.success) {
         setOpenEvent((event: EventFullDetails) => {
           event.transport = transport
+          event.transport.departTime = new Date(event.transport.departTime)
           return event
         })
       }
     }))
+  }
+
+  const addAttendee = (attendee) => {
+    fetchProtected(DASH_API + '/addAttendee', null, {...attendee, id: openEvent.event_id}, 'POST', (res) => {
+      if (res.success) {
+        setOpenEvent((event: EventFullDetails) => {
+          event.attendees.push(attendee)
+          return event
+        })
+      }
+    })
+  }
+
+  const editEvent = (event) => {
+    fetchProtected(DASH_API + '/editEvent', null, { ...event, id: openEvent.event_id}, 'PUT', (res) => {
+      if (res.success) {
+        setOpenEvent((currentEvent: EventFullDetails) => {
+          return {
+            ...currentEvent,
+            ...event,
+          }
+        })
+      }
+    })
   }
 
   const handleModalOpen = () => setModalOpen(true)
