0f76fb200c65f4fd5953d2245470db1828b5113c
Luke <lukebenjaminely@gmail.com>
2019-06-11 16:16:33 +0100

    Removed message on register

--- src/components/frontpage/Thanks.tsx
+++ src/components/frontpage/Thanks.tsx
@@ -12,7 +12,6 @@ const Thanks = (props) => {
     <div className='centered-panel'>
       <div className='login-panel'>
         <h1>Thanks for registering</h1>
-        <h3 className='grey-light'>Please check you email for your confirmation and password</h3>
         <Button variant='outlined' color='primary' className='chang-blue-font' onClick={gotoLogin}>
           Login
         </Button>
