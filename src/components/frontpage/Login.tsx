33d77c12d2ad0b83019e621506cb906205baf307
csh4017 <csh4017@ic.ac.uk>
2019-06-18 11:59:18 +0100

    Add more inspiration to the login screen

--- src/components/frontpage/Login.tsx
+++ src/components/frontpage/Login.tsx
@@ -5,11 +5,13 @@ import { Redirect } from 'react-router-dom'
 import { login } from '../../redux/actions/userActions'
 
 import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import IconButton from '@material-ui/core/IconButton'
 import InputAdornment from '@material-ui/core/InputAdornment'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
@@ -17,6 +19,8 @@ import './Login.scss'
 
 // tslint:disable-next-line:no-var-requires
 const loginImage = require('../../../assets/png/login.jpg')
+// tslint:disable-next-line:no-var-requires
+const logo = require('../../../assets/png/DashLogo-White@4x.png')
 
 const Login: React.FunctionComponent<LoginProps> = (props) => {
   const [visible, setVisibility] = React.useState<boolean>(false)
@@ -46,10 +50,63 @@ const Login: React.FunctionComponent<LoginProps> = (props) => {
 
   return props.loggedIn ? (
     <Redirect to='/' />
+  ) : props.vaporwave ? (
+    <div
+      style={{
+        background: '#008080',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Paper>
+        <div className='window-title-bar'>Enter Dash Password</div>
+        <div style={{ margin: '30px' }}>
+          <div>
+            <Typography>Type a user name and password to log on to Dash.</Typography>
+            <Typography>
+              Tip: if you don't have an account, you can <Link href='/register'>create one here</Link>.
+            </Typography>
+            <div style={{ marginTop: '30px', display: 'flex', alignItems: 'center' }}>
+              <Typography component='span'>User name</Typography>
+              <TextField
+                style={{ marginLeft: '15px' }}
+                variant='outlined'
+                type='text'
+                required={true}
+                error={props.error}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div style={{ marginTop: '30px', display: 'flex', alignItems: 'center' }}>
+              <Typography component='span'>Pasword</Typography>
+              <TextField
+                style={{ marginLeft: '15px' }}
+                variant='outlined'
+                type={visible ? 'text' : 'password'}
+                required={true}
+                error={props.error}
+                onChange={handlePasswordChange}
+                onKeyDown={handleKeyPress}
+              />
+            </div>
+          </div>
+          <Button variant='outlined' onClick={submit} color='primary' style={{ marginTop: '30px' }}>
+            Login
+          </Button>
+        </div>
+      </Paper>
+    </div>
   ) : (
     <div className='login-view'>
-      <img src={loginImage} className='left-panel' />
+      <div className='left-panel'>
+        <img src={loginImage} className='bkg' />
+        <img src={logo} className='logo' />
+      </div>
       <div className='right-panel'>
+        {props.waiting && <LinearProgress style={{ position: 'fixed', width: '50vw', top: '0', left: '50vw' }} />}
         <div className='login-panel'>
           <h3 className='large-heading'>Dash</h3>
           <h2 className='slogan'>Centralized Event Management</h2>
@@ -95,22 +152,19 @@ const Login: React.FunctionComponent<LoginProps> = (props) => {
             />
           </div>
           <div className='margin-bottom'>
-            {props.waiting ? (
-              <CircularProgress />
-            ) : (
-              <Button
-                variant='contained'
-                color='primary'
-                className='chang-blue-background'
-                onClick={submit}
-                size='large'
-                style={{
-                  fontWeight: 'bold',
-                }}
-              >
-                Login
-              </Button>
-            )}
+            <Button
+              variant='outlined'
+              disabled={props.waiting}
+              color='primary'
+              className='chang-blue-background'
+              onClick={submit}
+              size='large'
+              style={{
+                fontWeight: 'bold',
+              }}
+            >
+              Login
+            </Button>
           </div>
           <Link href='/register'>Don't have an account? Create one here</Link>
         </div>
@@ -123,6 +177,7 @@ interface LoginProps {
   waiting: boolean
   loggedIn: boolean
   error: boolean
+  vaporwave: boolean
   onLogin: (email: string, password: string) => void
 }
 
@@ -131,6 +186,7 @@ const mapStateToProps = (state) => {
     waiting: state.user.logginIn,
     loggedIn: state.user.loggedIn,
     error: state.user.error,
+    vaporwave: state.meme.vaporwave,
   }
 }
 
